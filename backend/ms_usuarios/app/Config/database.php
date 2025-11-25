174f2a1adf2d77f5874f2e9eec959f83c3a4448d
laura martinez <lsmartinezr@uniboyaca.edu.co>
2025-11-25 11:14:58 -0500

    intalacion del composer

---
+++ backend/ms_usuarios/app/Config/database.php
@@ -0,0 +1,10 @@
+$capsule->addConnection([
+    "driver" => "mysql",
+    "host" => "localhost",
+    "database" => "soporte_tickets",
+    "username" => "root",
+    "password" => "", 
+    "charset" => "utf8",
+    "collation" => "utf8_unicode_ci",
+    "prefix" => ""
+]);
